411b5b3740cc465d24535809ee6413148f00241f
Charison <charison.gillbranion@gmail.com>
2022-05-25 19:35:25 -0700

    Implement bytes to hex functions

--- s1/c2/FixedXor.c
+++ s1/c2/FixedXor.c
@@ -7,6 +7,14 @@
 int main()
 {
 	// TODO
+	const char hexStrIn[8] = "ABCDEF";
 	
+	unsigned char * buffer = make_byte_buff_from_hex_str(hexStrIn, strlen(hexStrIn));
+	const char * hexStrOut = make_hex_str_from_byte_buff(buffer, strlen(hexStrIn)/2);
+	
+	printf("Expected: %s\nGot: %s\n", hexStrIn, hexStrOut);
+	
+	free(buffer);
+	free((void *)hexStrOut);
 	return 0;
 }
--- s1/c2/FixedXor.h
+++ s1/c2/FixedXor.h
@@ -1,6 +1,48 @@
 #ifndef FIXED_XOR_H
 #define FIXED_XOR_H
 
+unsigned char hex_char_to_byte(const char hexChar)
+{
+	unsigned char byte = 0;
+	sscanf(&hexChar, "%hhx", &byte);
+	return byte;	// We only care about the least significant half of this byte.
+}
 
+char byte_to_hex_char(unsigned char byte)
+{
+	unsigned char hexChar[2] = { '\0', '\0' };
+	// printf("%hhx\n", byte);
+	sprintf(&(*hexChar), "%hhx", byte);
+	return *hexChar;	// We only care about the least significant half of this byte.
+}
+
+unsigned char * make_byte_buff_from_hex_str(const char * hexStr, int length)
+{
+	unsigned char lo, hi;
+	unsigned char * byteBuff = (unsigned char *)malloc(length/2);
+	
+	for(short i = 0; i < length; i+=2)
+	{
+		hi = hex_char_to_byte(hexStr[length-i-2]);	// Read the hex string backwards in pairs.
+		lo = hex_char_to_byte(hexStr[length-i-1]);	// Each converted character only occupies the back half of each byte.
+		byteBuff[(i/2)] = ( (hi << 4) | lo );		// Stuff all the half-bytes into contiguous blocks.
+	}
+	
+	return byteBuff;
+}
+
+const char * make_hex_str_from_byte_buff(unsigned char * byteBuff, int length)
+{
+	char lo, hi;
+	char * hexStr = (char *)malloc(length*2);
+	
+	for(short i = 0; i < length*2; i+=2)
+	{
+		hexStr[i] = byte_to_hex_char(byteBuff[length-1-(i/2)] >> 4);	// Read the hex string backwards in pairs.		
+		hexStr[i+1] = byte_to_hex_char(byteBuff[length-1-(i/2)] & 0x0F);	// Each converted character only occupies the back half of each byte.
+	}
+	
+	return (const char*)hexStr;
+}
 
 #endif // FIXED_XOR_H
