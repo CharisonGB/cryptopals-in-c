fd3d1a09b041fd1681e644e4d8bbdab83137a695
Charison <charison.gillbranion@gmail.com>
2022-05-27 17:40:23 -0700

    Implement core logic flow

--- s1/c3/ByteXorCypher.c
+++ s1/c3/ByteXorCypher.c
@@ -2,9 +2,67 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "ByteXorCypher.h"
+#include "../c2/XBDuplex.h"
+
+int find_max(int * values, int length)
+{
+	int maxIndex = 0, maxVal = values[maxIndex];
+	for(int i = 1; i < length; i++)
+	{
+		if(values[i] > maxVal)
+		{
+			maxIndex = i;
+			maxVal = values[i];
+		}
+	}
+	
+	return maxIndex;
+}
+
+unsigned char * make_filled_buffer(unsigned char val, int length)
+{
+	unsigned char * buffer = (unsigned char *)malloc(length);
+	
+	for(int i = 0; i < length; i++)
+		buffer[i] = val;
+	
+	return buffer; 
+}
+
+const char * make_reverse_str(const char * str)
+{
+	int length = strlen(str);
+	char * revStr = (char *)malloc(length+1);
+	
+	for(int i = 0; i < length; i++)
+		revStr[i] = str[length-i-1];
+	revStr[length] = '\0';
+	
+	return (const char *)revStr;
+}
+
 int main()
 {
-	// TODO
+	const char cyphertext[128] = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736";
 	
+	unsigned char * buffer = make_buffer_from_hex_str(cyphertext, strlen(cyphertext));
+	int * byteFreqBuff = CalculateByteFreq(buffer, strlen(cyphertext)/2);
+	
+	int likelyKey = find_max(byteFreqBuff, (int)BYTE_MAX);
+	
+	unsigned char * keyBuffer = make_filled_buffer(likelyKey, strlen(cyphertext)/2);
+	unsigned char * messageBuffer = buffer_xor_buffer(buffer, keyBuffer, strlen(cyphertext)/2);
+	
+	const char * message = make_reverse_str(messageBuffer+22); // FIXME: Spaces are null characters, message strings cut off early.
+	
+	// printf("Cyphertext: %s%c\n", buffer, '\0');
+	printf("Message: %s\n", message);
+	
+	free(buffer);
+	free(byteFreqBuff);
+	free(keyBuffer);
+	free(messageBuffer);
+	free((void *)message);
 	return 0;
 }
--- s1/c3/ByteXorCypher.h
+++ s1/c3/ByteXorCypher.h
@@ -1,8 +1,18 @@
 #ifndef BYTE_XOR_CYPHER
 #define BYTE_XOR_CYPHER
 
-// Count hex bytes
-// Read a byte buffer
-// Return a count buffer (shorts?)
+#include <stdlib.h>
+
+const size_t BYTE_MAX = 256;
+
+int * CalculateByteFreq(unsigned char * buffer, int length)
+{
+	int * freqBuffer = (int *)malloc(BYTE_MAX*sizeof(int));
+	
+	for(int i = 0; i < length; i++)
+		freqBuffer[buffer[i]]++;
+	
+	return freqBuffer;
+}
 
 #endif // BYTE_XOR_CYPHER
